1e19e0de7d5152860d3f9837949c34d35042dda1
MinGyu-Jo <ryzenkr@gmail.com>
2023-09-05 02:08:34 +0900

    230905_0208

--- src/main/java/com/springbootchatapi/controller/AccountInfoController.java
+++ src/main/java/com/springbootchatapi/controller/AccountInfoController.java
@@ -3,6 +3,8 @@ package com.springbootchatapi.controller;
 import com.springbootchatapi.account.AccountInformation;
 import com.springbootchatapi.member.Member;
 import com.springbootchatapi.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,29 +48,28 @@ public class AccountInfoController {
     }
 
 
-
-
-    /*
     public static class AccountInfo {
     }
-    */
 
-    /*
     @Autowired
     MemberRepository memberRepository;
 
     @GetMapping("/getAccountInfo")
     public Member getAccountInfo(HttpSession session) {
-        String sessionId = session.getId();
+        // String sessionId = session.getId();
 
+        String userId = (String) session.getAttribute("userId");
+        System.out.println("UserID from session: " + userId);                   // 로그를 통해 세션에서 가져온 ID 확인
+        return memberRepository.findById(userId).orElse(null);  // ID를 기반으로 회원 정보 조회
+        // 로그 구현을 위한 코드 추가
+        
         // sessionId를 기반으로 회원의 ID를 가져오는 로직
         // 이 부분은 세션을 어떻게 관리하고 있는지에 따라 다릅니다.
-        String memberId = "sessionId를_기반으로_memberId를_가져오는_로직";
+        // String memberId = "sessionId를_기반으로_memberId를_가져오는_로직";
 
-        Member member = memberRepository.findById(memberId).orElse(null);
-        return member;
+        // Member member = memberRepository.findById(memberId).orElse(null);
+        // return member;
     }
-     */
 
 
 }
